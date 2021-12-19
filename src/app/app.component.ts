9fe82031a140555a79eb7c772af56ee0eadaef8b
Saban Ünlü [netTrek] <us@netTrek.de>
2021-12-19 22:01:40 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'itd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'medienreich-it-designers';
+}
